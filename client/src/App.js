a565aed22365b3b9b07b03707e289a01a4ad5dd8
Eric Andrade <eric.andrade@gmail.com>
2018-08-27 08:59:10 -0400

    added nav, landing, and footer components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>RE: Connections is coming!</h1>
-        <div>
-        </div>
+        <Navbar />
+        <Landing />>
+        <Footer />
       </div>
     );
   }
