e473b3e961452b59cfbe4b83efd4482ed782f5f9
Eric Andrade <eric.andrade@gmail.com>
2018-08-27 10:27:23 -0400

    configured axios

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 class Register extends Component {
   constructor() {
@@ -28,7 +29,9 @@ class Register extends Component {
       passwordC: this.state.passwordC
     };
 
-    console.log(newUser);
+    axios.post('/api/users/register', newUser)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response.data))
   }
 
   render() {
