9b8bc3de491450ca0ad8700ec2c997f50d60a9fa
Eric Andrade <eric.andrade@gmail.com>
2018-08-28 10:43:39 -0400

    redux login action and user token setting

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,7 +1,10 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
 
-// reg user
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+
+// Register user
 export const registerUser = (userData, history) => dispatch => {
   axios
     .post("/api/users/register", userData)
@@ -12,4 +15,36 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Login - Get user token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      // save to local storage
+      const { token } = res.data;
+      // set token to local storage
+      localStorage.setItem("jwtToken", token);
+      // set token to auth header
+      setAuthToken(token);
+      // decode token to extract user data
+      const decoded = jwt_decode(token);
+      // set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
