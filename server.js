3af12c3527d4c07b0d12416288195af7a1ba5121
Eric Andrade <eric.andrade@gmail.com>
2018-09-07 06:56:04 -0400

    reconfigured port settings per heroku docs

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000;
+.listen(process.env.PORT || 5000);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
